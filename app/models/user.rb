6b13d78570ead75415644b266de03dc24e5368bf
Aldo Witzke <aldo_mwa@hotmail.com>
2020-03-20 16:17:22 -0300

    add dependent destroy to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_one :order
+  has_one :order, dependent: :destroy
   validates :cpf, presence: true, uniqueness: true, length: { minimum:11, maximum:11 }
   validates :email, presence: true, uniqueness: true
   accepts_nested_attributes_for :order, allow_destroy: true
