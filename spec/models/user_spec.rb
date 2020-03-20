c1b79de3b00986610c50274f545461d5920d817a
Aldo Witzke <aldo_mwa@hotmail.com>
2020-03-20 16:23:25 -0300

    fix user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   let(:user) { build(:user) }
   context "when user is valid" do
     it { is_expected.to validate_presence_of(:cpf) }
-    it { is_expected.to validate_uniqueness_of(:cpf).case_insensitive }
+    it { is_expected.to validate_uniqueness_of(:cpf) }
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email) }
     it { should have_one(:order) }
