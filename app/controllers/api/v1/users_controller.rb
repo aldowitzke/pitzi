9d2ee6d6f2c8ba90ad15c523c930f6e1ce926252
Aldo Witzke <aldo_mwa@hotmail.com>
2020-03-20 10:58:38 -0300

    add controllers

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :cpf, :email, order_attributes: [:price, :device, :imei, :installment])
+  end
+
+  def render_error
+    render json: { errors: @user.errors.full_messages },
+      status: :unprocessable_entity
+  end
+end
+
+
+
